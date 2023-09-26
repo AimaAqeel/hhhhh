3182c5a5fab45635bea44347901ad5b76725037f
AimaAqeel <144774523+AimaAqeel@users.noreply.github.com>
2023-09-26 02:20:25 -0700

    Add files via upload

---
+++ Chapter1/Ex2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 26 01:33:10 2023
+
+@author: AimsMughal
+"""
+
+#Exercise 2: Print the Version of Python
+#Write a Python program to get the Python version you are using.
+
+#The code imports the system's version and its version info and prints both as output
+import sys
+print("Python version")
+print(sys.version)
+print("Version info")
+print(sys.version_info)
